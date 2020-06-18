49c9b2da6cc18650a268b0251a1a284b34969e9d
visualfc <visualfc@gmail.com>
2020-06-18 21:27:09 +0800

    update example

--- static/playground.js
+++ static/playground.js
@@ -545,14 +545,24 @@ println(a, b, c)
 
 x := 3.14159265358979323846264338327950288419716939937510582097494459r
 x *= 2
-println(x)`)
+println(x)
+`)
 		break;
 		case "hello.txt":
 			setBody(`package main
 
-func main() {
-	println("Hello, Go+ ")
-}`)
+println("Hello, Go+")
+println(1r << 129)
+println(1/3r + 2/7r * 2)
+arr := [1, 3, 5, 7, 11, 13, 17, 19]
+println(arr)
+println([x*x for x <- arr, x > 3])
+m := {"Hi": 1, "Go+": 2}
+println(m)
+println({v: k for k, v <- m})
+println([k for k, _ <- m])
+println([v for v <- m])
+`)
 		break;
         }
 //        js_ajax("/doc/play/"+toy, {
