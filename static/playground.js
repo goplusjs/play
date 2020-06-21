be807c3959308d132ceabb7bfe336cf2e1dbcf44
visualfc <visualfc@gmail.com>
2020-06-21 19:06:56 +0800

    x

--- static/playground.js
+++ static/playground.js
@@ -511,9 +511,9 @@ function PlaygroundOutput(el) {
             shareURL.show().val(url).focus().select();
 
             if (rewriteHistory) {
-//             var historyData = {"code": sharingData};
-//             window.history.pushState(historyData, "", path);
-//             pushedEmpty = false;
+            var historyData = {"code": sharingData};
+            window.history.pushState(historyData, "", "");
+            pushedEmpty = false;
             }
           }
         }
