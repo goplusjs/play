d84a1c7d916edec411c4ccf4a52dc5c3662430de
visualfc <visualfc@gmail.com>
2020-08-02 11:55:44 +0800

    add repl iframe

--- build_latest.cmd
+++ build_latest.cmd
@@ -1,5 +1,6 @@
 @echo off
 go mod edit -require=github.com/goplus/gop@latest
-go list --tags js
+go list --tags="js repl"
 set GOOS=darwin
 gopherjs build -v -m -o goplus-play.js
+gopherjs build -v -m --tags repl -o repl.js repl.go
--- build_update.sh
+++ build_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-go mod edit -require=github.com/goplus/gop@latest
+go mod edit -require=github.com/goplus/gop@master
 go list --tags="js repl"
 gopherjs build -v -m -o goplus-play.js
 gopherjs build -v -m --tags repl -o repl.js repl.go
