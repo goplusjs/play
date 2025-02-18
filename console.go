082784ed3f7b593ebe00858accc882f906a02e18
visualfc <visualfc@gmail.com>
2025-02-18 15:32:08 +0800

    fix console

--- console.go
+++ console.go
@@ -7,8 +7,14 @@ import (
 func init() {
 	fn := js.Global().Get("console").Get("log")
 	js.Global().Get("console").Set("log", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		output = append(output, args[0].String()+"\n")
-		fn.Invoke(args[0])
+		ar := make([]interface{}, len(args))
+		for i, a := range args {
+			ar[i] = a
+		}
+		if len(args) > 0 {
+			output = append(output, args[0].String()+"\n")
+		}
+		fn.Invoke(ar...)
 		return nil
 	}))
 }
