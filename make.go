3fc1d16ae8dc5e71f4a2370eb4a593ee9e54c5b8
visualfc <visualfc@gmail.com>
2025-02-19 11:21:55 +0800

    hash files

--- make.go
+++ make.go
@@ -62,7 +62,8 @@ func check(err error) {
 
 func getHash() (string, error) {
 	h := md5.New()
-	for _, f := range []string{"main.go", "code.go", "console.go", "pkg_std.go", "loader_tpl.js", "go.mod"} {
+	for _, f := range []string{"main.go", "code.go", "console.go", "pkg_std.go",
+		"loader_tpl.js", "index_tpl.html", "go.mod"} {
 		data, err := ioutil.ReadFile(f)
 		if err != nil {
 			return "", err
