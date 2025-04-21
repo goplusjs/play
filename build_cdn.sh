1bd8ee186d745b5dbc8aebb51fc65e1901bc5c10
visualfc <visualfc@gmail.com>
2025-04-21 11:45:31 +0800

    x

--- build_cdn.sh
+++ build_cdn.sh
@@ -5,4 +5,4 @@ rm docs/*.wasm
 rm docs/static/playground_*.js
 #cp $GOROOT/misc/wasm/wasm_exec.js docs/
 cp wasm_exec_rt.js docs/
-go run make.go -domain https://play-static.gopluscdn.com/
+go run make.go -domain https://play-static.gopluscdn.com
